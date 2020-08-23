860e287444b6b5242eca61cc5091afc11a3b54ae
RoosterRoo <desayan9@gmail.com>
2020-08-23 18:52:27 +0530

    Use JSX in the components Pet and App

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me"),
-    React.createElement(Pet, {
-      name: "Scooby",
-      animal: "Dog",
-      breed: "Doberman",
-    }),
-    React.createElement(Pet, {
-      name: "Mickey",
-      animal: "Mouse",
-      breed: "Black Mouse",
-    }),
-    React.createElement(Pet, {
-      name: "Bugs",
-      animal: "Bunny",
-      breed: "Grey Rabbit",
-    }),
-  ]);
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, "Adopt me"),
+  //     React.createElement(Pet, {
+  //       name: "Scooby",
+  //       animal: "Dog",
+  //       breed: "Doberman",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Mickey",
+  //       animal: "Mouse",
+  //       breed: "Black Mouse",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Bugs",
+  //       animal: "Bunny",
+  //       breed: "Grey Rabbit",
+  //     }),
+  //   ]);
+  // };
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Scooby" animal="Dog" breed="Doberman" />
+      <Pet name="Mickey" animal="Mouse" breed="Black Mouse" />
+      <Pet name="Bugs" animal="Bunny" breed="Grey Rabbit" />
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
