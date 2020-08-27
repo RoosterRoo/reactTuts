2b05b27f1e795782e8bf2fc7f352b61b8caad8a6
RoosterRoo <desayan9@gmail.com>
2020-08-27 22:44:24 +0530

    created new file SearchParams

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h1", {}, "Adopt me"),
-  //     React.createElement(Pet, {
-  //       name: "Scooby",
-  //       animal: "Dog",
-  //       breed: "Doberman",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Mickey",
-  //       animal: "Mouse",
-  //       breed: "Black Mouse",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Bugs",
-  //       animal: "Bunny",
-  //       breed: "Grey Rabbit",
-  //     }),
-  //   ]);
-  // };
-
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Scooby" animal="Dog" breed="Doberman" />
+      {/* <Pet name="Scooby" animal="Dog" breed="Doberman" />
       <Pet name="Mickey" animal="Mouse" breed="Black Mouse" />
-      <Pet name="Bugs" animal="Bunny" breed="Grey Rabbit" />
+      <Pet name="Bugs" animal="Bunny" breed="Grey Rabbit" /> */}
+      <SearchParams />
     </div>
   );
 };
